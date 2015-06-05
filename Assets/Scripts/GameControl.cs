0d9b1a04c83c64789e49b08d014413ee893508a0
Jordi Santiago Provencio <metalzilla@gmail.com>
2015-06-05 12:13:07 +0200

    And more changes

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -6,13 +6,19 @@ public class GameControl : MonoBehaviour
 	public Camera MainCamera;
 	public RectTransform GameUI;
 	public RectTransform GameMenu;
+	public GameObject Playground;
 
-	BiribitManager manager;
-	bool showGameMenu = false;
+	RotateCamera m_rotateCamera = null;
+	BiribitManager m_manager;
+	bool m_showGameMenu = false;
+	int m_followingPlayer = -1;
+
+	Transform m_playerContainer = null;
+	Transform m_cameraPosition = null;
 
 	public void ToggleGameMenu()
 	{
-		showGameMenu = !showGameMenu;
+		m_showGameMenu = !m_showGameMenu;
 	}
 
 	void Start ()
@@ -20,7 +26,10 @@ public class GameControl : MonoBehaviour
 		if (MainCamera == null)
 			MainCamera = Camera.main;
 
-		manager = BiribitManager.Instance;
+		if (MainCamera != null)
+			m_rotateCamera = MainCamera.GetComponent<RotateCamera>();
+
+		m_manager = BiribitManager.Instance;
 	}
 	
 	void FixedUpdate()
@@ -28,7 +37,7 @@ public class GameControl : MonoBehaviour
 		if (GameUI != null)
 		{
 			Vector2 pivot = GameUI.pivot;
-			if (manager.IsConnected())
+			if (m_manager.IsConnected() && m_manager.HasJoinedRoom())
 				pivot.x = Mathf.Lerp(pivot.x, 1.0f, 0.1f);
 			else
 				pivot.x = Mathf.Lerp(pivot.x, 0.0f, 0.1f);
@@ -39,16 +48,55 @@ public class GameControl : MonoBehaviour
 		if (GameMenu != null)
 		{
 			Vector2 pivot = GameMenu.pivot;
-			if (showGameMenu)
+			if (m_showGameMenu)
 				pivot.y = Mathf.Lerp(pivot.y, 1.0f, 0.1f);
 			else
 				pivot.y = Mathf.Lerp(pivot.y, 0.0f, 0.1f);
 
 			GameMenu.pivot = pivot;
 		}
+
+		if (m_cameraPosition != null)
+		{
+			MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, m_cameraPosition.position, 0.1f);
+			MainCamera.transform.rotation = Quaternion.Lerp(MainCamera.transform.rotation, m_cameraPosition.rotation, 0.1f);
+		}
+		else
+		{
+			Vector3 initPos = new Vector3(0, 3, 0);
+			MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, initPos, 0.1f);
+		}
 	}
 
-	void Update () {
-	
+	void Update ()
+	{
+		bool connected = m_manager.IsConnected();
+		if (m_rotateCamera != null)
+			m_rotateCamera.enabled = !connected;
+
+		if (connected)
+		{
+			uint localPlayer = m_manager.GetLocalPlayer();
+			if (m_followingPlayer != localPlayer)
+			{
+				if (Playground != null)
+				{
+					string playerName = "Player" + (localPlayer + 1);
+					Debug.Log("Finding " + playerName);
+					m_playerContainer = Playground.transform.Find("Player " + (localPlayer + 1));
+					if (m_playerContainer != null)
+						m_cameraPosition = m_playerContainer.Find("CameraPosition");
+					else
+						m_cameraPosition = null;
+				}
+			}
+		}
+		else
+		{
+			m_followingPlayer = -1;
+			m_playerContainer = null;
+			m_cameraPosition = null;
+		}
+		
 	}
 }
--- Assets/Scripts/Interactable/PlayerCard.cs
+++ Assets/Scripts/Interactable/PlayerCard.cs
@@ -3,29 +3,34 @@ using System.Collections;
 
 public class PlayerCard : Interactable
 {
-	struct State
-	{
-		Vector3 position;
-	}
+
+	Vector3 positionToGo = Vector3.zero;
 
 	protected override void DoStateTransition(SelectionState state, bool instant)
 	{
 		switch (state)
 		{
 			case SelectionState.Normal:
-				///
+				positionToGo = Vector3.zero;
+				GetComponent<Renderer>().material.SetFloat("_Bright", 1);
+				GetComponent<Renderer>().material.SetFloat("_Saturation", 1);
 				break;
 			case SelectionState.Highlighted:
-				///
+				positionToGo = new Vector3(0, 0.1f, 0);
+				GetComponent<Renderer>().material.SetFloat("_Bright", 1.5f);
+				GetComponent<Renderer>().material.SetFloat("_Saturation", 1);
 				break;
 			case SelectionState.Pressed:
-				///
+				positionToGo = new Vector3(0, 0.2f, 0);
+				GetComponent<Renderer>().material.SetFloat("_Bright", 2);
+				GetComponent<Renderer>().material.SetFloat("_Saturation", 1);
 				break;
 			case SelectionState.Disabled:
-				///
+				positionToGo = Vector3.zero;
+				GetComponent<Renderer>().material.SetFloat("_Saturation", 0);
 				break;
 			default:
-				///
+				positionToGo = Vector3.zero;
 				break;
 		}
 	}
@@ -35,7 +40,8 @@ public class PlayerCard : Interactable
 	}
 	
 	
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+		transform.localPosition = Vector3.Lerp(transform.localPosition, positionToGo, 0.1f);
 	}
 }
